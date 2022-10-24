15ee9d295fababb1fb4395c492ec6665c1c7c555
Abhay Sharma <abhaysharmajr@gmail.com>
2022-10-24 22:48:02 +0530

    hide SQL client

--- sqlConfig.js
+++ sqlConfig.js
@@ -1,16 +1,16 @@
 import mysql from "mysql";
 
 //Mysql connection
-async function createSqlConnection() {
-  const SqlClient = mysql.createConnection({
-    host: "localhost",
-    port: "3306",
-    user: "root",
-    password: "8854892348abSH*",
-    database: "abhaydb",
-  });
-  SqlClient.connect();
-  console.log("Mysql connection established");
-  return SqlClient;
-}
-export const SqlClient = await createSqlConnection();
+// async function createSqlConnection() {
+//   const SqlClient = mysql.createConnection({
+//     host: "localhost",
+//     port: "3306",
+//     user: "root",
+//     password: "8854892348abSH*",
+//     database: "abhaydb",
+//   });
+//   SqlClient.connect();
+//   console.log("Mysql connection established");
+//   return SqlClient;
+// }
+// export const SqlClient = await createSqlConnection();
